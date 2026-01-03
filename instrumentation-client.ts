95dc81ed667518ccfbd878cc320c1677df028299
Tharak Nagaveti <tharak.nagaveti@gmail.com>
2026-01-03 18:31:32 +0530

    Remove deprecated experimental.instrumentationHook from next.config.js and update dependencies

---
+++ instrumentation-client.ts
@@ -0,0 +1,8 @@
+// instrumentation-client.ts
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2025-11-30'
+});
+
